e4b8f33718b4964b33f08b5f909078c63bb85f77
RJGATON007 <robertgaton2017@gmail.com>
2025-09-20 15:43:32 +0800

    created a PUT and PATCH Method for the Books app

--- books/views.py
+++ books/views.py
@@ -10,11 +10,10 @@ class BookViewSet(viewsets.ModelViewSet):
     search_fields = ['genre', 'title', 'author']
     ordering_fields = ['published_date', 'genre', 'title', 'author', 'created_at']
 
-    # Handle GET /api/v1/books/
+
     def list(self, request, *args, **kwargs):
         queryset = self.filter_queryset(self.get_queryset())
 
-        # Example: filter by genre with ?genre=FIC
         genre = request.query_params.get('genre')
         if genre:
             queryset = queryset.filter(genre=genre)
@@ -30,3 +29,44 @@ class BookViewSet(viewsets.ModelViewSet):
         book = self.get_object()
         serializer = self.get_serializer(book)
         return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            self.perform_create(serializer)  # save to DB
+            return Response(
+                {
+                    "message": "Book created successfully",
+                    "book": serializer.data
+                },
+                status=status.HTTP_201_CREATED
+            )
+        return Response(
+            {
+                "message": "Book creation failed",
+                "errors": serializer.errors
+            },
+            status=status.HTTP_400_BAD_REQUEST
+        )
+    
+    
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+
+        if serializer.is_valid():
+            self.perform_update(serializer)
+            return Response(
+                {"message": "Book updated successfully", "book": serializer.data},
+                status=status.HTTP_200_OK
+            )
+        return Response(
+            {"message": "Book update failed", "errors": serializer.errors},
+            status=status.HTTP_400_BAD_REQUEST
+        )
+    
+    
+    
+    
--- django_rest_main/settings.py
+++ django_rest_main/settings.py
@@ -133,7 +133,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 # Global Pagination
 REST_FRAMEWORK = {
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
-    "PAGE_SIZE": 4,  # Default page size for pagination
+    "PAGE_SIZE": 2,  # Default page size for pagination
     "DEFAULT_FILTER_BACKENDS": ["django_filters.rest_framework.DjangoFilterBackend"],
     "SEARCH_PARAM": "q",
     "ORDERING_PARAM": "order-by",
